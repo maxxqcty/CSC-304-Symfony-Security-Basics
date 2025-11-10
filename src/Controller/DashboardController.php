2cc390649d5fa3816eafdf7e15940c879e8ed687
maxxqcty <enolpeaxlian120@gmail.com>
2025-11-11 05:32:09 +0800

    updated dashboard controller

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -4,27 +4,20 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-// #[IsGranted('ROLE_USER')] -- controller protection method
 final class DashboardController extends AbstractController
 {
     #[Route('/dashboard', name: 'app_dashboard')]
-    // public function index(): Response
-    // {
-    //     return new Response("Welcome to the dashboard! (Protected Area)");
-    // }
-
+    #[IsGranted('ROLE_USER')] // ensures only users with ROLE_USER (or ROLE_ADMIN via hierarchy) can access
     public function index(): Response
     {
         $user = $this->getUser();
 
-        if ($this->isGranted('ROLE_ADMIN')) {
-            $message = 'You are logged in as Admin. You have full access.';
-        } else {
-            $message = 'You are logged in as Regular User. Limited access granted.';
-        }
+        $message = $this->isGranted('ROLE_ADMIN')
+            ? 'You are logged in as Admin. You have full access.'
+            : 'You are logged in as Regular User. Limited access granted.';
 
         return $this->render('dashboard/index.html.twig', [
             'message' => $message,
