ebcd59d27947a32e89bcea0e5a94aa2445c566b8
maxxqcty <enolpeaxlian120@gmail.com>
2025-11-11 05:30:02 +0800

    updated security providers

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -5,12 +5,30 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+// #[IsGranted('ROLE_USER')] -- controller protection method
 final class DashboardController extends AbstractController
 {
     #[Route('/dashboard', name: 'app_dashboard')]
+    // public function index(): Response
+    // {
+    //     return new Response("Welcome to the dashboard! (Protected Area)");
+    // }
+
     public function index(): Response
     {
-        return new Response("Welcome to the dashboard! (Protected Area)");
+        $user = $this->getUser();
+
+        if ($this->isGranted('ROLE_ADMIN')) {
+            $message = 'You are logged in as Admin. You have full access.';
+        } else {
+            $message = 'You are logged in as Regular User. Limited access granted.';
+        }
+
+        return $this->render('dashboard/index.html.twig', [
+            'message' => $message,
+            'username' => $user->getUserIdentifier(),
+        ]);
     }
 }
